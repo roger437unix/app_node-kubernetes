01a561dd1c14c283eb86389818b102b852fd3e8a
roger437unix <33252885+roger437unix@users.noreply.github.com>
2024-04-05 12:47:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8080
+const IP = require('ip');
+
+app.get('/', (req, res) => {
+    const ipAddress = IP.address();
+    res.send('<h1 style="text-align:center;color:#ffac56">'+ipAddress+'</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`\nApp Node IPv4 listening on port ${port}`)
+  console.log(`\nhttp://localhost:${port}`)
+})
